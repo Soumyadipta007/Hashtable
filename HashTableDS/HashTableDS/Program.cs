a7af31b3f8ec2c1a5cbe7982e8d4fa442d0d0913
Soumya <soumyadipta0077@gmail.com>
2020-11-01 14:05:35 +0530

    [Soumya] Add: Finding Frequency

---
+++ HashTableDS/HashTableDS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HashTableDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			MyMapNode<String, int> myHashMap = new MyMapNode<String, int>(5);
+			String sentence = "to be or not to be";
+			String[] words = sentence.ToLower().Split(" ");
+			foreach (String word in words)
+			{
+				Console.WriteLine(word);
+				int value = myHashMap.Get(word);
+				if (value == 0)
+					value = 1;
+				else
+					value = value + 1;
+				myHashMap.Add(word, value);
+			}
+			int frequency = myHashMap.Get("be");
+			Console.WriteLine("Frequency- " + frequency);
+        }
+    }
+}
